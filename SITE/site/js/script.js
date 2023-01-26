ba822e71652fbc095025a14f071ef398938f0f1b
Hiago Bernardo <105991097+Brazoo@users.noreply.github.com>
2023-01-26 18:14:14 -0300

    Add files via upload

---
+++ SITE/site/js/script.js
@@ -0,0 +1,8 @@
+function dados() {
+  let user = document.getElementById("user").value;
+  let password = document.getElementById("pass").value;
+  console.log("user:" + " " + user, "password:" + " " + password);
+};
+	function goBack() {
+		window.history.back()
+	}

